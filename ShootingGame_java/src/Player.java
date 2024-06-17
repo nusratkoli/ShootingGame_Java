dcc34cec077a598d147e95035f0af790079e283e
Koli <zeal23310183@fsgcl.jp>
2024-06-17 10:56:13 +0900

    10.4-10.5

--- ShootingGame_java/src/Player.java
+++ ShootingGame_java/src/Player.java
@@ -1,10 +1,43 @@
 
-public class Player  extends Character{
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+public class Player  extends Character implements KeyListener{
 	public Player (double x, double y, double vx, double vy)
 	{
 		//
 		super(x,y,vx,vy);
 	}
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=-5;
+		}
+		if (e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			vx=5;
+			}
+		
+		if (e.getKeyCode()==KeyEvent.VK_SPACE) {
+			GameWorld.playerBullets.add(new PlayerBullet(x,y,0,-10));
+		}
+	}
+	public void keyRelrased(KeyEvent e) {
+		if (e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=0;}
+		
+		if (e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=0;}
+	}
 
-	
+	public void keyTyped(KeyEvent e) {}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+	public void move() {
+		super.move();
+		if(x<0)
+			x=0;
+		if(x>370)
+			x=370;
+	}
 }
