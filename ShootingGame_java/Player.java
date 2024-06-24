ce5ea8f0fa73981dcf284ba403c41e4b77d31ba9
Koli <zeal23310183@fsgcl.jp>
2024-06-24 10:20:34 +0900

    11.4

---
+++ ShootingGame_java/Player.java
@@ -0,0 +1,54 @@
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+public class Player  extends Character implements KeyListener{
+	
+	public Player(double x, double y, double vx, double vy) {
+		super(x, y, vx, vy);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public void draw (MyFrame f){
+		f.setColor(0, 128,0);
+		f.fillRect(x, y+20, 30, 10);
+		f.setColor(200, 200,0);
+		f.fillRect(x+10, y, 10, 30);
+		
+		
+	}
+	
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=-5;
+		}
+		if (e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			vx=5;
+			}
+		
+		if (e.getKeyCode()==KeyEvent.VK_SPACE) {
+			
+			GameWorld.playerBullets.add(new PlayerBullet(x,y,5,-10));
+			GameWorld.playerBullets.add(new PlayerBullet(x,y,0,-10));
+			GameWorld.playerBullets.add(new PlayerBullet(x,y,-5,-10));
+			
+			System.out.println("の数"+GameWorld.playerBullets.size());
+		}
+	}
+	public void keyRelrased(KeyEvent e) {
+		if (e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=0;}
+		
+		if (e.getKeyCode()==KeyEvent.VK_LEFT) {
+			vx=0;}
+	}
+
+	public void keyTyped(KeyEvent e) {}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+	
+	
+
+}
---
+++ ShootingGame_java/PlayerBullet.java
@@ -0,0 +1,14 @@
+
+public class PlayerBullet extends Character{
+	public PlayerBullet(double x, double y, double  vx, double vy)
+	{
+		// Character
+		super (x,y,vx,vy);
+	}
+	public void draw (MyFrame f) {
+		f.setColor(0,128,0);
+		f.fillRect(x, y+20, 30, 10);
+		
+	}
+
+}
