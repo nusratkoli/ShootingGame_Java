7473b96922366055269a3ac18f62e5d0033a7584
Koli <zeal23310183@fsgcl.jp>
2024-06-24 10:48:36 +0900

    12.1

--- ShootingGame_java/src/GameWorld.java
+++ ShootingGame_java/src/GameWorld.java
@@ -3,5 +3,7 @@ import java.util.Vector;
 public class GameWorld {
 	static Player player;
 	static Vector<PlayerBullet>playerBullets;
+	static Vector<Enemy> enemies;
+	public static Vector<Enemy> enemies;
 
 }
